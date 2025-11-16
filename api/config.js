8a0e43ba5fd4b4478dec081c5663075380467bf6
mqteuss <mh.umateus@gmail.com>
2025-11-16 12:23:48 -0300

    Atualizar o config.js

--- api/config.js
+++ api/config.js
@@ -3,15 +3,15 @@
 export default async function handler(request, response) {
     
     // Pega as chaves do ambiente Vercel
-    const { SUPABASE_URL, SUPABASE_KEY } = process.env;
+    const { SUPABASE_URL, SUPABASE_ANON_KEY } = process.env;
 
-    if (!SUPABASE_URL || !SUPABASE_KEY) {
+    if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
         return response.status(500).json({ error: "Variáveis do SupABASE não configuradas no servidor." });
     }
 
     // Envia as chaves públicas para o cliente
     return response.status(200).json({
         supabaseUrl: SUPABASE_URL,
-        supabaseKey: SUPABASE_KEY // Esta é a chave 'anon' (pública), é seguro enviá-la
+        supabaseKey: SUPABASE_ANON_KEY // Esta é a chave 'anon' (pública), é seguro enviá-la
     });
 }

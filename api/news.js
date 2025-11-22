018c28b714d2adc44a91f1f97789cbcfe67f80d2
mqteuss <mh.umateus@gmail.com>
2025-11-21 21:01:15 -0300

    Update news.js

--- api/news.js
+++ api/news.js
@@ -37,7 +37,7 @@ export default async function handler(request, response) {
     contents: [{ parts: [{ text: `Encontre 8 notícias recentes de FIIs (${todayString})` }] }],
     tools: [{ google_search: {} }], 
     generationConfig: {
-      temperature: 0.2,
+      temperature: 0.3,
       responseMimeType: "application/json", // O 2.0 SUPORTA ISSO COM TOOLS
       responseSchema: NEWS_SCHEMA
     },
